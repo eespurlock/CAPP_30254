0213fb877fe698fc4eeeb0d59b562496383ae865
Esther Spurlock <eespurlock@cs.uchicago.edu>
2019-04-06 13:04:37 -0500

    getting further in

--- assignment1/diagnostic.py
+++ assignment1/diagnostic.py
@@ -7,72 +7,36 @@ Due: 4/9/2019
 '''
 #imports
 import pandas as pd
-import os.path
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-#Constants
-
-#The following CSV comes from the the below URL
-#The filters for this data are dates after 12/31/2016 11:59:59 PM and before
-#12/21/2018 11:59:59 PM
-#https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present/ijzp-q8t2/
-#data
-CSV_NAME = "Crimes_2017_2018_data.csv"
-
-#The following are the constants for the column names for problem 1
-ID = "ID"
-CASE_NUM = "Case Number"
-DATE = "Date"
-BLOCK = "Block"
-IUCR = "IUCR"
-PRIMARY = "Primary Type"
-DES = "Description"
-LOC_DES = "Location Description"
-ARREST = "Arrest"
-DOMESTIC = "Domestic"
-BEAT = "Beat"
-DISTRICT = "District"
-WARD = "Ward"
-COMMUNITY = "Community Area"
-FBI_CODE = "FBI Code"
-X_CO = "X Coordinate"
-Y_CO = "Y Coordinate"
-YEAR = "Year"
-UPDATED = "Updated On"
-LAT = "Latitude"
-LON = "Longitude"
-LOCATION = "Location"
-
-#Problem 1
-def read_crimes_csv(csvfile=CSV_NAME):
+def read_crimes_csv():
     '''
-    This function reads in the crimes data from a CSV and then cleans the data
+    This function reads in the crimes data from a CSV and then analyzes the data
     
-    Input: csvfile: the name of a CSV file
+    The following CSV comes from the the below URL
+    The filters for this data are dates after 12/31/2016 11:59:59 PM and before
+    12/21/2018 11:59:59 PM
+    https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present/
+    ijzp-q8t2/data
     '''
-    if os.path.exists(csvfile):
-        df_crimes = pd.read_csv(csvfile)
-    else:
-        #Should exit out of the function if the path doesn't exist.
-    	print("No path to this CSV")
-    	return None
+    #Problem 1
+    df_crimes = pd.read_csv("Crimes_2017_2018_data.csv")
+    #This will give you counts of all the different kinds of crimes
+    counted_df = df_crimes["Primary Type"].value_counts()
+    counted_df.plot()
+    #This will show you how crimes change over time
+    year_crimes = df_crimes.groupby(["Year","Primary Type"]).size()
+    year_crimes.plot()
+    #This will show you how crimes differ by block
+    block_crimes = df_crimes.groupby(["Block", "Primary Type"]).size()
+    block_crimes.plot()
 
-    summary_stats(df_crimes)
+    #Problem 3
 
-def summary_stats(df_crimes):
-	'''
-	Creates summary statistics from the crime datarame
-
-	Input: df_crimes: a pandas dataframe
-	'''
-	#This will give you counts of all the different kinds of crimes
-	counted_df = df_crimes[PRIMARY].value_counts()
-	#This will show you how crimes change over time
-	year_crimes = df_crimes.groupby(["Year","Primary Type"]).size()
-	#This will show you how crimes differ by block
-	block_crimes = df_crimes.groupby(["Block", "Primary Type"]).size()
-
-	return counted_df, year_crimes, block_crimes
-
-#Problem 2
+    df_crimes["Date"]= pd.to_datetime(df_crimes["Date"])
+    df_crimes["Month"] = df_crimes["Date"].dt.month
+    filt = df_crimes["Month"].isin([7])
+    df_filt = df_crimes[filt]
+    filt = df_filt["Primary Type"].isin(["ROBBERY", "BATTERY", "BURGLARY",
+    	"MOTOR VEHICLE THEFT"])
+    df_filt = df_filt[filt]
+    df_filt.groupby(["Year", "Primary Type"]).size()
