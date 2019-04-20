d8d5c37fc0f744870e50924dfbbd02bdd90364f5
Esther Spurlock <eespurlock@cs.uchicago.edu>
2019-04-20 11:15:43 -0500

    safety

--- group_proj/early_data_exp.py
+++ group_proj/early_data_exp.py
@@ -7,12 +7,13 @@ Early Data Exploration
 import pandas as pd
 
 #Constants
-rcra_csvs = ['RCRA_ENFORCEMENTS.csv',
-'RCRA_EVALUATIONS.csv',
-'RCRA_FACILITIES.csv',
-'RCRA_NAICS.csv',
+rcra_csvs = ['RCRA_NAICS.csv',
 'RCRA_VIOLATIONS.csv',
-'RCRA_VIOSNC_HISTORY.csv']
+'RCRA_VIOSNC_HISTORY.csv',
+'RCRA_ENFORCEMENTS.csv',
+'RCRA_EVALUATIONS.csv']
+
+cant_download = ['RCRA_FACILITIES.csv']
 
 def download_data(csv_lst=rcra_csvs):
     '''
@@ -22,7 +23,9 @@ def download_data(csv_lst=rcra_csvs):
         print(curr_csv)
         df_data = pd.read_csv(curr_csv)
         print(df_data.shape)
-        print(df_data.columns)
-        for col in  df_data.columns:
+        colnames = df_data.columns
+        print(colnames)
+        for col in colnames:
             curr_series = df_data[col]
             print(curr_series.describe())
+            print(curr_series.value_counts().size)
